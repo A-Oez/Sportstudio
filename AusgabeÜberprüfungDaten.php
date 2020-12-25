57f5562a7226825ce78851f37b92c458e4dfed1c
A-Oez <60219737+A-Oez@users.noreply.github.com>
2020-12-25 14:35:19 +0100

    Update AusgabeÜberprüfungDaten.php

--- AusgabeÜberprüfungDaten.php
+++ AusgabeÜberprüfungDaten.php
@@ -3,45 +3,127 @@
 ?>
 
 <html>
-    <head>
-        <title>Überprüfung der eingegebenen Daten</title>
-    </head>
-    <body>
-        <h1>Sind die angegebene Werte richtig?</h1>
 
-        <p>Ausgewähltes Studio: <?php echo $_POST["FormSelectStudio"]?></p>
-        <p>Ausgewälter Tarif: <?php echo $_POST["FormSelectTarif"]?></p>
-        <p>Vorname: <?php echo $_POST["inputVName"]?></p>
-        <p>Nachname: <?php echo $_POST["inputNName"]?></p>
-        <p>Geschlecht: <?php echo $_POST["FormSelectGender"]?></p>
-        <p>Geburtsdatum: <?php echo $_POST["inputGeburtstag"]?></p>
-        <p>Straße: <?php echo $_POST["inputStraße"]?></p>
-        <p>HausNr.: <?php echo $_POST["inputHN"]?></p>
-        <p>PLZ: <?php echo $_POST["inputPLZ"]?></p>
-        <p>Ort: <?php echo $_POST["inputOrt"]?></p>
-        <p>Email: <?php echo $_POST["inputEmail"]?></p>
-        <p>Newsletter: 
-        <?php 
-        if(isset($_POST["newsletterTRUE"]))
-        {
-            $newsletter = 1;
-            echo "Ja";
-        }        
-        else
-        {
-            $newsletter = 0;
-            echo "Nein";
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Daten- überprüfen</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <style>
+        html {
+            overflow-x: hidden;
         }
-        ?></p>
-        <p>Telefon: <?php echo $_POST["inputTLF"]?></p>
-        <p>Kontoinhaber: <?php echo $_POST["inputKontoinhaber"]?></p>
-        <p>IBAN: <?php echo $_POST["inputIBAN"]?></p>
+    </style>
+</head>
 
+<body>
+    <nav class="navbar" style="transform: translate(15%); left: 10%;">
+        <ul class="nav navbar-nav navbar-right">
+            <li><img src="images/logo3.png"></li>
+        </ul>
+    </nav>
 
-         <!-- Button --> 
-        <from action="AusgabeÜberprüfungDaten.php" method="post">
-            <input type="submit" name="test" id ="test" value="An Datenbank übertragen"/> 
+    <h1 style="text-align: center;">Sind die angegebene Werte richtig?</h1>
+    <form>
+        <div class="container">
+            <div class="form-row">
+                <div class="form-group col-md-6 ">
+                    <label for="FormSelectStudio_">Ausgewählter Standort:</label>
+                    <input type="text" class="form-control" name="FormSelectStudio_" readonly value="<?php echo $_POST["FormSelectStudio"]?>">
+                </div>
+                <div class="form-group col-md-6 ">
+                    <label for="FormSelectTarif_">Ausgewählter Standort:</label>
+                    <input type="text" class="form-control" name="FormSelectTarif_" readonly value="<?php echo $_POST["FormSelectTarif"]?>">
+                </div>
+            </div>
+
+            <div class="form-row">
+                <div class="form-group col-md-6 ">
+                    <label for="inputVName_">Vorname:</label>
+                    <input type="text" class="form-control" name="inputVName_" readonly value="<?php echo $_POST["inputVName"]?>">
+                </div>
+                <div class="form-group col-md-6 ">
+                    <label for="inputNName_">Nachname:</label>
+                    <input type="text" class="form-control" name="inputNName_" readonly value="<?php echo $_POST["inputNName"]?>">
+                </div>
+            </div>
+
+            <div class="form-row">
+                <div class="form-group col-md-6 ">
+                    <label for="FormSelectGender_">Geschlecht:</label>
+                    <input type="text" class="form-control" name="FormSelectGender_" readonly value="<?php echo $_POST["FormSelectGender"]?>">
+                </div>
+                <div class="form-group col-md-6 ">
+                    <label for="inputGeburtstg_">Geburtsdatum:</label>
+                    <input type="text" class="form-control" name="inputGeburtstg_" readonly value="<?php echo $_POST["inputGeburtstag"]?>">
+                </div>
+            </div>
+
+            <div class="form-row">
+                <div class="form-group col-md-6 ">
+                    <label for="inputStraße_">Straße:</label>
+                    <input type="text" class="form-control" name="inputStraße_" readonly value="<?php echo $_POST["inputStraße"]?>">
+                </div>
+                <div class="form-group col-md-6 ">
+                    <label for="inputHN_">Hausnummer:</label>
+                    <input type="text" class="form-control" name="inputHN_" readonly value="<?php echo $_POST["inputHN"]?>">
+                </div>
+            </div>
+
+            <div class="form-row">
+                <div class="form-group col-md-6 ">
+                    <label for="inputPLZ_">PLZ:</label>
+                    <input type="text" class="form-control" name="inputPLZ_" readonly value="<?php echo $_POST["inputPLZ"]?>">
+                </div>
+                <div class="form-group col-md-6 ">
+                    <label for="inputOrt_">Ort:</label>
+                    <input type="text" class="form-control" name="inputOrt_" readonly value="<?php echo $_POST["inputOrt"]?>">
+                </div>
+            </div>
+
+            <div class="form-row">
+                <div class="form-group col-md-6 ">
+                    <label for="inputEmail_">Email:</label>
+                    <input type="text" class="form-control" name="inputEmail_" readonly value="<?php echo $_POST["inputEmail"]?>">
+                </div>
+                <div class="form-group col-md-6 ">
+                    <label for="newsletterTRUE_">Newsletter:</label>
+                    <input type="text" class="form-control" name="newsletterTRUE_" readonly value="<?php 
+                        if(isset($_POST[" newsletterTRUE "]))
+                        {
+                            $newsletter = 1;
+                            echo "Ja ";
+                        }        
+                        else
+                        {
+                            $newsletter = 0;
+                            echo "Nein ";
+                        }
+                        ?>">
+                </div>
+                <div class="form-group col-md-6 ">
+                    <label for="inputTLF_">Telefonnummer:</label>
+                    <input type="text" class="form-control" name="inputTLF_" readonly value="<?php echo $_POST["inputTLF"]?>">
+                </div>
+            </div>
+
+            <div class="form-row">
+                <div class="form-group col-md-6 ">
+                    <label for="inputKontoinhaber_">Kontoinhaber:</label>
+                    <input type="text" class="form-control" name="inputKontoinhaber_" readonly value="<?php echo $_POST["inputKontoinhaber"]?>">
+                </div>
+                <div class="form-group col-md-6 ">
+                    <label for="inputIBAN_">IBAN:</label>
+                    <input type="text" class="form-control" name="inputIBAN_" readonly value="<?php echo $_POST["inputIBAN"]?>">
+                </div>
+            </div>
+    </form>
+        <form action="AusgabeÜberprüfungDaten.php" method="post">
+          <input type="submit" class="btn btn-primary " name="test" id="test" value="An Datenbank übertragen" />
         </form>
+    </div>
 
         <?php 
             $servername = "localhost";
@@ -84,5 +166,42 @@
                 $con->close();
             }
         ?>
-    </body>
+
+        <footer class="bg-light ">
+            <div class="container p-4 ">
+                <div class="row ">
+                    <div class="col-lg-3 col-md-6 mb-4 mb-md-0; text-center " style="margin-left: 250px ">
+                        <h5 class="text-uppercase ">Kontakt</h5>
+                        <ul class="list-unstyled mb-0 ">
+                            <li>
+                                <a class="text-dark ">Telefonnummer: 01572-3400569</a>
+                            </li>
+                            <li>
+                                <a class="text-dark ">InfoSportstudioWolkenfeld@gmail.com </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-3 col-md-6 mb-4 mb-md-0; text-center " style="margin-left: 50px; ">
+                        <h5 class="text-uppercase mb-0 ">Anschrift (Zentralsitz)</h5>
+                        <ul class="list-unstyled ">
+                            <li>
+                                <a class="text-dark ">Sportstudio Wolkenfeld</a>
+                            </li>
+                            <li>
+                                <a class="text-dark ">Neue Gartenstraße 12</a>
+                            </li>
+                            <li>
+                                <a class="text-dark ">74072 Heilbronn</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="text-center p-3 " style="background-color: steelblue; margin-right: auto; ">
+                © 2020 Copyright:
+                <a class="text-dark ">Sportsudio-Wolkenfeld</a>
+            </div>
+        </footer>
+</body>
+
 </html>
